12c2e6957f5af9288427897af09b3530c969051c
Aashir Israr <aashirisrar968@gmail.com>
2024-02-13 23:58:23 +0500

    Update links in Hero component

--- components/hero.tsx
+++ components/hero.tsx
@@ -53,13 +53,13 @@ const Hero = () => {
       <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-10">
         <a
           className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 rounded-[6px]"
-          href="/docs"
+          href="/caption-generator"
         >
           Get Started
         </a>
         <a
           className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 rounded-[6px]"
-          href="/components"
+          href="/hastag-generator"
         >
           Components
         </a>
